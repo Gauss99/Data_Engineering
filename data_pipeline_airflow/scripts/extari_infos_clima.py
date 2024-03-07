14b4738036a4934a9773d27f0e72606d916ae04d
Gustavo Joaquim Brito dos Santos <gustavojbs98@gmail.com>
2024-03-06 21:38:40 -0300

    add weather data from API

---
+++ data_pipeline_airflow/scripts/extari_infos_clima.py
@@ -0,0 +1,29 @@
+import os
+from os.path import join
+import pandas as pd
+from datetime import datetime, timedelta
+
+# intervalo de datas
+data_inicio = datetime.today()
+data_fim = data_inicio + timedelta(days=7)
+
+# formatando as datas
+data_inicio = data_inicio.strftime('%Y-%m-%d')
+data_fim = data_fim.strftime('%Y-%m-%d')
+
+city = 'Boston'
+key = 'SF6EUSXVKZGCUM2TRGSQSWP67'
+
+URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+            f'{city}/{data_inicio}/{data_fim}?unitGroup=metric&include=days&key={key}&contentType=csv')
+
+
+dados = pd.read_csv(URL)
+print(dados.head())
+
+file_path = f'/home/gustavojoaquim/Documents/Data_Engineering/data_pipeline_airflow/csv_files/semana={data_inicio}/'
+os.mkdir(file_path)
+
+dados.to_csv(file_path + 'dados_brutos.csv')
+dados[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(file_path + 'temperaturas.csv')
+dados[['datetime', 'description', 'icon']].to_csv(file_path + 'condicoes.csv')
